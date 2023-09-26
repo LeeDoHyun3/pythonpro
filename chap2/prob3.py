88ab86470e7ea722ad43d97d3b2914cf737ee140
LeeDohyun <vmao1111@naver.com>
2023-09-26 22:45:21 +0900

    final

--- chap2/prob3.py
+++ chap2/prob3.py
@@ -1,3 +1,17 @@
 name = input("Hi. What's your name? ")
-age = input("And how old are you?")
+age = int(input("And how old are you? "))
+weigh = int(input("Okay, last question. How many kg do you weigh? "))
+
+dogAge = int(age / 6)
+print("\nDid you know taht you're just " + str(dogAge) + " in dog years?\n")
+
+AgeToSeconds = age * 365 * 24 * 60 * 60
+print("But you're also " + str(AgeToSeconds) + " seconds old.\n")
+
+print("If a small child were trying to get your attention, your name would become : \n" + (name * 5))
+
+print("\nDid you know that on the moon you would weigh only "+ str(weigh / 6) + " pounds?")
+print("But on the sun, you'd weigh " + str(weigh * 27.1) + " <but, ah... not for long>.\n\n")
+print("Press the enter key to exit.")
+
 
