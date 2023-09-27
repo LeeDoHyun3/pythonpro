b625692c4d5e5860a9dae0ac11d43ec0ee643aaa
LeeDohyun <vmao1111@naver.com>
2023-09-27 01:09:07 +0000

    sdf

--- chap2/prob3.py
+++ chap2/prob3.py
@@ -1,3 +1,7 @@
 name = input("Hi. What's your name? ")
 age = input("And how old are you?")
+weight = input("Okay, last qustion. ow many kg do you weight?")
+
+dogAge = int(age) / 7
+print("\nDid you know that you're just " + dogAge + " in dog years?")
 
